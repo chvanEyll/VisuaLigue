791955a8bae09814d9604be394b212d55d12a848
grasshop <chvaneyll@gmail.com>
2017-01-07 11:17:51 -0500

    deleted folders

--- VisuaLigue/src/main/java/ca/ulaval/glo2004/visualigue/utils/math/easing/EasingFunction.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo2004.visualigue.utils.math.easing;
-
-public interface EasingFunction {
-
-    double ease(double startValue, double endValue, double elapsedTime, double duration);
-}
